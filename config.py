ad7a9acba4af2297eca0e49c7f46854bcffe737b
Alla Moiseenko <allamoiseenko24@gmail.com>
2024-10-27 13:59:23 +0300

    Удалил config.py из отслеживания и добавил в .gitignore

--- config.py
+++
@@ -1,4 +0,0 @@
-base_url = "https://discord.com/api/v10"
-api_key = "Bot MTI4NjYwOTgxOTEzNzk5ODg4Mw.GqnGVM.RqVfcEJ445tTP11YROADiwqqHJj_mexQq_wf3A"
-chanel_id = "1281160246110457922"
-user_id = "1286609819137998883"
